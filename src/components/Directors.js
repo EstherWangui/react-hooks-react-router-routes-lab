0a4d8735390f0b438308925e6099cc7b13f971b2
EstherWangui <esther.nganga@student.moringaschool.com>
2022-09-30 06:59:45 +0300

    completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  //return <div>{/*{code here}*/}</div>;
+  let directorsCards = directors.map(director => (
+    <div key={director.name}>
+      Name: {director.name}
+      Movies:
+      <ul>
+        {director.movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+    </div>
+  ))
+
+
+  return (
+    <>
+    <h1>Directors Page</h1>
+    {directorsCards}
+    </>
+  )
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,23 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+ //return <div>{/*{code here}*/}</div>;
+ let movieCard = movies.map(movie => (
+  <div key={movie.title}>
+    Name: {movie.title}<br/>
+    Time: {movie.time}
+    <ul>
+      {movie.genres.map(genre => <li key={genre}>{genre}</li>)}
+    </ul>
+  </div>
+))
+  return (
+      <>
+     <h1>Movies Page</h1>
+      {movieCard}
+
+      </>
+  )
 }
 
 export default Movies;
